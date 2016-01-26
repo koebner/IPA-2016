e48e8bbd12e8a7abd535579f57e8077dbf028de4
koebner <auzpit@hotmail.com>
2016-01-25 18:21:46 -0600

    mi primera aplicacion Java en IntelliJ

---
+++ miSegundaAplicacion/src/hola2/ipa2016.java
@@ -0,0 +1,14 @@
+package hola2;
+
+/**
+ * Created by e-okcenter on 25/01/2016.
+ */
+public class ipa2016 {
+
+    public static void main(String[] args){
+
+        System.out.println("Hola soy una app");
+
+
+    }
+}
